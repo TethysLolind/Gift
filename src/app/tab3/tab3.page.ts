d694cdf129d5f63233d385436d36e2039ceb32c2
Bo Zhang <BZhang26@slb.com>
2019-10-28 18:16:38 +0800

    add service worker update

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
+import {Network} from '@ngx-pwa/offline';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+  $hasNetwork: Observable<boolean>;
+  constructor(private networkService: Network) {
+
+  }
+  ngOnInit() {
+    this.$hasNetwork = this.networkService.onlineChanges;
+  }
+}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ApplicationRef, OnDestroy } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
+import { AlertController } from '@ionic/angular';
+import { Subscription, interval, concat } from 'rxjs';
+import {first} from 'rxjs/operators';
+import { AlertOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage implements OnInit, OnDestroy {
+  constructor(private updateService: SwUpdate,
+    private alertControl: AlertController,
+    private appRef: ApplicationRef) {
+  }
+  subscriptions: Subscription[] = [];
+
+  doNothing;
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
+  }
+  ngOnInit(): void {
+    this.initUpdater();
+  }
+
+  initUpdater() {
+    const updateTimer$ = interval(1000 * 60 * 0.5);
+    const isAppStable$ = this.appRef.isStable.pipe(first(isStable => isStable === true));
+    const appStableInterval$ = concat(updateTimer$, isAppStable$); // 先interval一下,再检查一下stable
+
+
+    this.subscriptions.push(this.updateService.available.subscribe(e => this.onUpdateAvailable(e)));
+    this.subscriptions.push(this.updateService.available.subscribe(e => this.onUpdateActivated(e)));
+    this.subscriptions.push(appStableInterval$.subscribe(() => this.checkForUpdate()));
+  }
+  onUpdateActivated(e: UpdateAvailableEvent): void {
+    throw new Error('Method not implemented.');
+  }
+
+  checkForUpdate() {
+    if (this.updateService.isEnabled) {
+      await this.updateService.checkForUpdate();
+    }
+  }
+  onUpdateAvailable(event: UpdateAvailableEvent) {
+    console.log(event);
+    let opts: AlertOptions;
+    opts = {
+      header: 'Update Received',
+      // subHeader?: string;
+      message: 'Updating is Ready, proceed to update',
+      // cssClass?: string | string[];
+      // inputs?: AlertInput[];
+      buttons: [{
+        text: 'Not Now',
+        role: 'cancel',
+        handler: async () => { await Promise.resolve(); }
+      },
+      {
+        text: 'OK',
+        handler: async () => {await this.updateService.activateUpdate(); window.location.reload(); }
+      }
+    ],
+      // backdropDismiss?: boolean;
+      translucent: true,
+      animated: true,
+      // mode?: Mode;
+      keyboardClose: true,
+      // id?: string;
+      // enterAnimation?: AnimationBuilder;
+      // leaveAnimation?: AnimationBuilder;
+    };
+    const alert = await this.alertControl.create(opts);
+    alert.present();
+  }
+
+}
