fa1ca7139bfda803f3867571e4c740b5700cea9d
Bo Zhang <BZhang26@slb.com>
2019-11-09 12:06:32 +0800

    88

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
-  signalrUrl: 'http://localhost:5000/api/msgHub',
-  chatApi: 'http://localhost:5000/api/chat',
-  clearMsg: 'clearMsg'
-  // signalrUrl: 'https://giftxin.azurewebsites.net/api/msgHub',
-  // chatApi: 'https://giftxin.azurewebsites.net/api/chat',
+  clearMsg: 'clearMsg',
+  signalrUrl: 'https://giftxin.azurewebsites.net/api/v1/msgHub',
+  chatApi: 'https://giftxin.azurewebsites.net/api/v1/chat',
 };
