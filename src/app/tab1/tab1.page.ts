6c4a625f69ba3521551baf0e4d39d86e192635e0
Bo Zhang <BZhang26@slb.com>
2019-11-10 16:46:39 +0800

    add first

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,4 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  public id1 = '01';
+  public id2 = '01';
+
+  public id3 = '01';
+  public id4 = '01';
+  constructor() {
+   this.id1 = this.generatePicId();
+   this.id2 = this.generatePicId();
+   this.id3 = this.generatePicId();
+   this.id4 = this.generatePicId();
+  }
+
+  private generatePicId() {
+    const idnumber = Math.round(Math.random() * 34);
+    let idstring = idnumber.toString();
+    if (idnumber < 10) {
+      idstring = '0' + idnumber;
+    }
+    return idstring;
+  }
+}
